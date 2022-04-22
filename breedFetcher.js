51ce51addcd31fc7604c164eb32fd49cf60be32b
mariannebourcier <marianne.bourcier@gmail.com>
2022-04-22 13:22:25 -0600

    refactor function, create index file, add mocha and chai for testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,19 @@
 const request = require('request');
 
-let breed = process.argv.slice(2);
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('There was an error:', error);
-  }
-  const data = JSON.parse(body);
-  if (breed) {
-    console.log(data[0].description);
-  } else {
-    console.log('Breed not found!');
-  }
-});
 
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('There was an error:', error);
+    }
+    const data = JSON.parse(body);
+    if (breedName) {
+      console.log(data[0].description);
+    } else {
+      console.log('Breed not found!');
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
